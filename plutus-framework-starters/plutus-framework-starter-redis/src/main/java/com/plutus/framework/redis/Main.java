441f40b4e3bc8948aa41b095498f91b1c4b4a4b5
lijian232 <lijian232@LIJIAN232819>
2023-06-28 17:54:46 +0800

    [init] 初始化项目

---
+++ plutus-framework-starters/plutus-framework-starter-redis/src/main/java/com/plutus/framework/redis/Main.java
@@ -0,0 +1,13 @@
+package com.plutus.framework.redis;
+
+/**
+ * @ClassName:${NAME}
+ * @Auther: lijian
+ * @Date: ${DATE} ${HOUR}:${MINUTE}
+ * @Description: $Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
